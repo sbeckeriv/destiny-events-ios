4f90af4a12989328790ca6acb344dc0ae606f9ca
Stephen Becker IV <github@deathbyescalator.com>
2015-03-12 22:12:35 -0700

    displays

--- Podfile
+++ Podfile
@@ -1,12 +1,11 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '6.0'
 
+pod 'Firebase', '>= 2.2.0'
 target 'destiny' do
-
 end
 
 target 'destinyTests' do
-  pod 'Firebase', '>= 2.2.0'
 
 end
 
--- destiny/DataViewController.swift
+++ destiny/DataViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
-
+class DataViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    var items: [String] = ["We", "Heart", "Swift"]
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: AnyObject?
-
+    @IBOutlet weak var tableView: UITableView!
+    var refreshControl:UIRefreshControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refersh")
+        self.refreshControl.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.addSubview(refreshControl)
+        let appDelegate =
+    
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -32,7 +43,27 @@ class DataViewController: UIViewController {
             self.dataLabel!.text = ""
         }
     }
+    func refresh(sender:AnyObject)
+    {
+        dispatch_async(dispatch_get_main_queue()) {
+        }
+    }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
 
