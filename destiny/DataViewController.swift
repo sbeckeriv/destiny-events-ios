3056b717bc9c5fa3045f527881f87fa8ef50340f
Stephen Becker IV <github@deathbyescalator.com>
2015-03-14 09:47:49 -0700

    kinda works

--- destiny/DataViewController.swift
+++ destiny/DataViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Foundation
 class DataViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     var items: [String] = []
-var fireItems = Dictionary<String, Dictionary<String, AnyObject> >()
+    var fireItems = [String: [AnyObject]]()
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: AnyObject?
     @IBOutlet weak var tableView: UITableView!
@@ -49,20 +49,44 @@ var fireItems = Dictionary<String, Dictionary<String, AnyObject> >()
     func loadFromFirebase(){
         myRootRef.observeEventType(.Value, withBlock: { snapshot in
             self.fireItems.removeAll()
-            var now : NSDate
             for rest in snapshot.children.allObjects as [FDataSnapshot] {
                 let key_date : String = "2015-07-16 03:03:34"
                 var dateFormatter = NSDateFormatter()
                 dateFormatter.dateFormat =
                 "yyyy-MM-dd HH:mm:ss"
                 var date:NSDate! = dateFormatter.dateFromString(key_date)
-                println("-----")
                 if (date != nil){
                     let elapsedTimeSeconds = NSDate().timeIntervalSinceDate(date)
                     let minutesLapsed = -1 * (elapsedTimeSeconds/60)
                 }
-                println(rest.value)
-                self.fireItems[rest.key as String] = rest.value as Dictionary
+                var planetName = ""
+                var eventList = [[String]]()
+                for child in rest.children.allObjects as [FDataSnapshot]{
+                    if (child.key == "planetName"){
+                        planetName = child.value as String
+                    }
+                    if(child.key == "mapLocations"){
+                        println(child.children.allObjects as [FDataSnapshot])
+                        for events in child.children.allObjects as [FDataSnapshot]{
+                            
+                            var event = [String]()
+                            var types:[String] = events.value["eventTypes"] as [String]
+                            
+                            event.append(events.value["title"] as String)
+                            //event.append("|".join(types))
+                            var time = events.value["start"] as String
+                            var now = NSDate()
+                            var timeInt:Int! = time.toInt()
+                            var date = now + timeInt.seconds
+                            let elapsedTimeSeconds = NSDate().timeIntervalSinceDate(date)
+                            let minutesLapsed = -1 * (elapsedTimeSeconds/60)
+                            event.append("\(Int(minutesLapsed)) minutes")
+                            eventList.append(event)
+                        }
+                    }
+                }
+                println(planetName)
+                self.fireItems[planetName as String] = eventList
             }
             self.buildItems()
             self.tableView.reloadData()
@@ -71,9 +95,15 @@ var fireItems = Dictionary<String, Dictionary<String, AnyObject> >()
     }
     func buildItems(){
         self.items.removeAll()
-        for (key, value) in self.fireItems {                println(key)
-            
-            self.items.append("\(key) => \(value)")
+        for (key, value) in self.fireItems {
+            println(key)
+            println(value)
+            if(value.count>0){
+                for event in value {
+                var j = " ".join(event as [String])
+                self.items.append("\(key): \(j)")
+                }
+            }
         }
 
     }
